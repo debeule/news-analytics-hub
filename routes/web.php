e10e71119152f83ae656fcceb775763312a9e4bd
debeule <debeulematthias@gmail.com>
2023-12-12 11:58:19 +0100

    rest

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\Test2Controller;
 use App\Http\Controllers\GetDashboardController;
 use App\Http\Controllers\ScrapeArticlesListController;
 
-Route::get('/', GetDashboardController::class)->name('dashboard');
+
 Route::get('/test', TestController::class)->name('test');
+Route::get('/test2', Test2Controller::class)->name('test2');
+
+Route::get('/', GetDashboardController::class)->name('dashboard');
 Route::get('/dispatch', ScrapeArticlesListController::class)->name('dispatch');
