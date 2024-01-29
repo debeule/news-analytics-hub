f546a61832b14691f92266c322840b4f92dc46f8
debeule <debeulematthias@gmail.com>
2024-01-29 11:58:30 +0100

    Added demorgen / fixed index typo

--- config/scraping.php
+++ config/scraping.php
@@ -2,11 +2,16 @@
 
 return [
     'organizations' => [
-            "0" => [
-                "name" => "hln",
+            "1" => [
+                "name" => "detijd",
+                "organization_type" => "news_paper",
+            ],
+            "2" => [
+                "name" => "demorgen",
                 "organization_type" => "news_paper",
             ],
     ],
+    
     
     'destination' => env('SCRAPER_PROJECT_DESTINATION'),
     'enableVenvCommand' => env('APP_ENV') == 'local' ? ".\\venv\\Scripts\\activate" : "source .\\venv\\Scripts\\activate",
