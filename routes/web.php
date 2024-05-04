1aee03275610eeb43aba1d92fe1e5a56f0c1c732
debeule <debeulematthias@gmail.com>
2024-05-04 18:01:59 +0200

    Removed unused migrations

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Router;
+
 use Http\Endpoints\TestHandler;
 use Http\Endpoints\ScrapeArticlesListHandler;
 
-
 Route::get('/test', TestHandler::class)->name('test');
 
-Route::get('/', function () {
+Route::get('/', function () 
+{
     return view('dashboard');
-})->name('dashboard');
+});
 
 Route::get('/dispatch', ScrapeArticlesListHandler::class)->name('dispatch');
