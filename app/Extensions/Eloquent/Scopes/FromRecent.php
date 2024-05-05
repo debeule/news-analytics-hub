486dfe849302c56a185b90e42c8a9706c64202c5
debeule <debeulematthias@gmail.com>
2024-05-05 12:03:12 +0200

    Added value object for Datetime

--- app/Extensions/Eloquent/Scopes/FromRecent.php
+++ app/Extensions/Eloquent/Scopes/FromRecent.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace App\Extensions\Eloquent\Scopes;
 
-use App\Imports\Values\Date;
+use App\Imports\Values\Datetime;
 use Illuminate\Database\Eloquent\Builder;
 
 final class FromRecent
 {
     public function __construct(
-        private Date $date = new Date,
+        private Datetime $Datetime = new Datetime,
     ) {}
 
     public function __invoke(Builder $query): Builder
     {
         return $query
-            ->whereDate('article_created_at', '>=', $this->date->toString());
+            ->whereDatetime('article_created_at', '>=', $this->Datetime->toString());
     }
 }
