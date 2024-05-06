9ed3cabc7006a06e8d40e1472b03ca98bebc5221
debeule <debeulematthias@gmail.com>
2024-05-06 09:18:55 +0200

    Simplified import article interface

--- app/Imports/Queries/Article.php
+++ app/Imports/Queries/Article.php
@@ -4,11 +4,7 @@ namespace App\Imports;
 
 interface Article 
 {
-    public function getMainTitle(): string;
-    public function getUrl(): string;
-    public function getFullContent(): string;
-    public function getCategory(): string;
-    public function getAuthor(): string;
-    public function getOrganization(): string;
-    public function getArticleCreatedAt(): \DateTime;
+    public function title(): string;
+    public function url(): string;
+    public function fullContent(): string;
 }
