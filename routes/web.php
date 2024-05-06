0c9ecf460cd5c1178e7e75396973514ac78a1a26
debeule <debeulematthias@gmail.com>
2024-05-06 09:41:52 +0200

    Temporary changes

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Route;
 use Http\Endpoints\TestHandler;
 use Http\Endpoints\ScrapeArticlesListHandler;
 
-Route::get('/test', TestHandler::class)->name('test');
-
-Route::get('/', function () 
-{
-    return view('dashboard');
-});
+Route::get('/', TestHandler::class)->name('test');
 
 Route::get('/dispatch', ScrapeArticlesListHandler::class)->name('dispatch');
