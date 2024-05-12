4a8c71efcc4a61d21ebee09b9c1b7adb34bf3f01
debeule <debeulematthias@gmail.com>
2024-05-13 00:43:19 +0200

    Updated article interface

--- app/Imports/Queries/Article.php
+++ app/Imports/Queries/Article.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App\Imports;
+namespace App\Imports\Queries;
+
+use App\Newspaper\Organization;
 
 interface Article 
 {
     public function title(): string;
     public function url(): string;
     public function fullContent(): ?string;
-    public function organization(): string;
+    public function organization(): Organization;
 }
--- app/Imports/Queries/ExternalArticles.php
+++ app/Imports/Queries/ExternalArticles.php
@@ -1,12 +1,10 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Imports\Queries;
 
 use Illuminate\Support\Collection;
 
 interface ExternalArticles
 {
-    public function get(): Collection;
+    public function get(): collection;
 }
