861613cb4796230104163e432b2a3e5836db5df4
debeule <debeulematthias@gmail.com>
2024-05-13 00:45:10 +0200

    Fixed scrape commands

--- app/Scraper/Commands/ScrapeArticle.php
+++ app/Scraper/Commands/ScrapeArticle.php
@@ -2,38 +2,46 @@
 
 declare(strict_types=1);
 
-namespace App\Raw;
+namespace App\Scraper\Commands;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use GuzzleHttp\Psr7\Response;
+
+use App\Scraper\Article;
+use App\Imports\Values\ScrapeArticleEndpoint;
+use App\Services\PostRequest;
 
 class ScrapeArticle implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public function __construct(
-        private int $scrapingUrl,
+        private Article $article,
         private ScrapeArticleEndpoint $endpoint = new ScrapeArticleEndpoint(),
     ) {}
 
-    public static function setup(string $url): self
+    public static function setup(Article $article): self
     {
-        return new self($url);
+        return new self($article);
     }
 
-    public function execute(): void
+    public function execute(): Response
     {
-        PostRequest::setup(
+        return PostRequest::setup(
             (string) $this->endpoint,
-            ['url' => $this->scrapingUrl]
+            [
+                'organization_id' => $this->article->organizationId,
+                'url' => $this->article->url,
+            ]
         )->execute();
     }
 
-    public function get()
+    public function get(): Response
     {
-        return $this->execute()->full_content;
+        return $this->execute();
     }
 }
--- app/Scraper/Commands/ScrapeArticlesList.php
+++ app/Scraper/Commands/ScrapeArticlesList.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Scraper\Commands;
 
-use App\Newspaper\Article;
+use App\Scraper\Article;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -13,6 +13,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Collection;
 
 use App\Imports\Values\ScrapeArticlesListEndpoint;
+use App\Imports\Values\Response;
 use App\Services\PostRequest;
 
 class ScrapeArticlesList implements ShouldQueue
@@ -29,23 +30,26 @@ class ScrapeArticlesList implements ShouldQueue
         return new self($organizationId);
     }
 
-    public function execute(): void
+    public function execute()
     {
-        PostRequest::setup(
+        return PostRequest::setup(
             (string) $this->endpoint,
-            ['organizationId' => $this->organizationId]
+            ['organization_id' => $this->organizationId]
         )->execute();
-    }
+    }   
 
     public function get(): Collection
     {
+        $response = $this->execute();
+        $data = Response::fromResponse($response)->getData();
+
         $collection = collect();
 
-        foreach($this->execute() as $listItem)
+        foreach($data['response'] as $listItem)
         {
-            $externalArticle = new Article($listItem->title, $listItem->url, $listItem->organization_id);
+            $externalArticle = new Article($listItem['title'], $listItem['url'], $this->organizationId);
 
-            $collection->push(new Article($externalArticle));
+            $collection->push($externalArticle);
         }
 
         return $collection;
