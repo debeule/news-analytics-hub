81e70004fcc382a65ade9a7a73fb0b27c4f7d93d
debeule <debeulematthias@gmail.com>
2024-05-13 15:10:03 +0200

    Added processing prompt to scraping config

--- config/scraping.php
+++ config/scraping.php
@@ -12,7 +12,11 @@ return [
             ],
     ],
     
-    
-    'destination' => env('SCRAPER_PROJECT_DESTINATION'),
-    'enableVenvCommand' => env('APP_ENV') == 'local' ? ".\\venv\\Scripts\\activate" : "source .\\venv\\Scripts\\activate",
+    'processing' => [
+        'prompt' => "Respond only with an array. Do not exclude data. Everything in lowercase. Structure the array like: 
+
+            key: 'categories' value: category of news discussed.
+            key: 'entities-occupations' value: array containing all entities mentioned with: [entity, occupation of entity, location (if not mentioned => null), organization of entity (not applicable = null)].
+            key: 'entity-mentions' value: array containing all instances of an entity being mentioned with: [mentioned entity, context of mention, location(not mentioned = null), positivity score from 1 - 16]",
+    ]
 ];
