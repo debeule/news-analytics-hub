9e8e2b9a93dc11712b91e64ebe14ddb7aa15ab13
debeule <debeulematthias@gmail.com>
2024-05-14 12:54:02 +0200

    Added created_at key to openai prompt

--- config/scraping.php
+++ config/scraping.php
@@ -18,6 +18,9 @@ return [
             key: category
             value: choose most fitting: Politics, Business, Economy, Technology, Science & planet, Entertainment, Sports, Opinion,
            
+            key: created_at
+            value: date article was published in format yyyy-mm-dd-hh-mm
+
             key: organizations
             value: every mentioned organizations and company 
                 with 
