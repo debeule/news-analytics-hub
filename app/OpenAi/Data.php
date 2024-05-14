d4b63b0eb09bcc82ad9466ad9539faf2eed62546
debeule <debeulematthias@gmail.com>
2024-05-14 11:37:08 +0200

    Temporary commit

---
+++ app/OpenAi/Data.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\OpenAi;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Imports\Queries\Article as ArticleInterface;
+
+class Article implements ArticleInterface
+{
+    public function __construct(
+        public string $title, 
+        public string $url, 
+        public int $organizationId,
+        public ?string $fullContent = null
+    ) {}
+    
+    public function title(): string
+    {
+        return $this->title;
+    }
+
+    public function url(): string
+    {
+        return $this->url;
+    }
+
+    public function organizationId(): int
+    {
+        return $this->organizationId;
+    }
+
+    public function fullContent(): string
+    {
+        return $this->fullContent;
+    }
+}
