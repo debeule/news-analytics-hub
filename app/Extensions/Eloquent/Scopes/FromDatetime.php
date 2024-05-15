a6da52227cfda1e7aabbad898face4b5388228d9
debeule <debeulematthias@gmail.com>
2024-05-15 23:55:36 +0200

    Renamed Datetime to DateTime

--- app/Extensions/Eloquent/Scopes/FromDatetime.php
+++ app/Extensions/Eloquent/Scopes/FromDatetime.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace App\Extensions\Eloquent\Scopes;
 
-use App\Imports\Values\Datetime;
+use App\Imports\Values\DateTime;
 use Illuminate\Database\Eloquent\Builder;
 
-final class FromDatetime
+final class FromDateTime
 {
     public function __construct(
-        private Datetime $Datetime = new Datetime,
+        private DateTime $DateTime = new DateTime,
     ) {}
 
     public function __invoke(Builder $query): Builder
     {
         return $query
-            ->whereDate('article_created_at', '>=', $this->Datetime->toString());
+            ->whereDate('article_created_at', '>=', $this->DateTime->toString());
     }
 }
