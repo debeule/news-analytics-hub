5a375b5fe83f43c108cdc75c09786cae06342517
debeule <debeulematthias@gmail.com>
2024-05-15 23:56:15 +0200

    Added test to ProcessArticle

---
+++ app/Imports/ProcessArticleTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Imports;
+
+use App\Imports\ProcessArticle;
+use App\Article\Queries\ArticleByTitle;
+use App\OpenAi\Commands\ProcessData;
+use App\OpenAi\Commands\CreateDataObject;
+use App\Imports\Dtos\Article as ExternalArticle;
+use App\Article\Commands\ProcessArticleDomain;
+use App\Article\Commands\ProcessEntityDomain;
+use App\Article\Commands\ProcessMentionDomain;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Queue;
+use App\Testing\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use Illuminate\Support\Facades\Bus;
+use Mockery\MockInterface;
+use Mockery;
+
+use Database\OpenAi\DataFactory;
+
+class ProcessArticleTest extends TestCase
+{
+    #[Test]
+    public function CanProcessArticleWithProcessedData()
+    {
+        Bus::fake();
+
+        $data = DataFactory::new()->create();
+
+        $processArticle = new ProcessArticle($data->article());
+
+        $processArticleMock = Mockery::mock(ProcessArticle::class, [$data->article()])->makePartial();
+        $processArticleMock->shouldReceive('getData')->once()->andReturn($data);
+
+        $processArticleMock->handle();
+
+        Bus::assertDispatchedSync(ProcessArticleDomain::class);
+        Bus::assertDispatchedSync(ProcessEntityDomain::class);
+        Bus::assertDispatchedSync(ProcessMentionDomain::class);
+    }
+}
