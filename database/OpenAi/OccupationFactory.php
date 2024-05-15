c4d79303779b85158b18bd2ecf840d15a9e1e552
debeule <debeulematthias@gmail.com>
2024-05-15 23:33:12 +0200

    Added factory for OpenAi Occupation DTO

---
+++ database/OpenAi/OccupationFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\OpenAi;
+
+use App\OpenAi\Occupation;
+use Faker\Factory as FakerFactory;
+use Illuminate\Support\Collection;
+
+final class OccupationFactory
+{
+    public function __construct(
+        private Collection $occupations,
+    ){}
+
+    public static function new()
+    {
+        return new self(
+            collect([self::build()])
+        );
+    }
+
+    public static function build(): Occupation
+    {
+        $faker = FakerFactory::create();
+
+        return new Occupation(
+            $faker->name(),
+        );
+    }
+
+    public function count(int $times): self
+    {
+        for ($i = 0; $i < $times - 1; $i++) 
+        {
+            $this->occupations->push($this->build());
+        }
+
+        return new self($this->occupations);
+        
+    }
+
+    public function create()
+    {
+        if($this->occupations->count() === 1) 
+        {
+            return $this->occupations->first();
+        }
+
+        return $this->occupations;
+    }
+}
