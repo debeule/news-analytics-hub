5309c9e71d64b379d161f45c67e4f5e730f308e4
debeule <debeulematthias@gmail.com>
2024-05-15 23:56:50 +0200

    Updated jobs for new structure

--- app/Imports/ProcessArticle.php
+++ app/Imports/ProcessArticle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Newspaper\Commands;
+namespace App\Imports;
 
 use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
@@ -8,32 +8,39 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 
 use App\OpenAi\Commands\ProcessData;
-use App\Imports\Queries\Article as ExternalArticle;
+use App\OpenAi\Commands\CreateDataObject;
+use App\Imports\Dtos\Article as ExternalArticle;
 use App\Article\Commands\ProcessArticleDomain;
+use App\Entity\Commands\ProcessEntityDomain;
+use App\Mention\Commands\ProcessMentionDomain;
+use App\OpenAi\Data;
 
-final class ProcessArticle implements ShouldQueue
+class ProcessArticle implements ShouldQueue
 {
-    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use DispatchesJobs, InteractsWithQueue, Queueable, SerializesModels, Batchable;
+
+    public ProcessData $processData;
 
     public function __construct(
         public ExternalArticle $article,
-        private ArticleByTitle $articleByTitle = new ArticleByTitle(),
-    ){}
+    ){
+        $this->processData = new ProcessData($this->article->fullContent());
+    }
 
-    public function __invoke(): void
+    public function handle(): void
     {
-        $data = ProcessData::setup($this->article->fullContent)->execute();
+        $data = $this->getData();
 
-        $this->article->article_created_at = $data['created_at'];
-        $this->article->category = $data['category'];
+        $this->dispatchSync(new ProcessArticleDomain($data->article()));
+        $this->dispatchSync(new ProcessEntityDomain($data));
+        $this->dispatchSync(new ProcessMentionDomain($data));
+    }
 
-        $this->dispatchSync(new ProcessArticleDomain($this->article));
-
-        $article = $this->articleByTitle->hasTitle($this->article->title)->find();
-        
-        $this->dispatchSync(new ProcessEntityDomain($data, $article->id));
-        $this->dispatchSync(new ProcessMentionDomain($data, $article->id));
+    public function getData(): Data
+    {
+        return CreateDataobject::fromArray($this->processData->get(), $this->article)->toDataObject();
     }
 }
--- app/Imports/SyncAllSources.php
+++ app/Imports/SyncAllSources.php
@@ -9,13 +9,13 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-
+use Illuminate\Foundation\Bus\DispatchesJobs;
 
 use App\Article\Commands\SyncArticles;
 
 final class SyncAllSources implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use DispatchesJobs, InteractsWithQueue, Queueable, SerializesModels;
 
     public function handle(): void
     {
