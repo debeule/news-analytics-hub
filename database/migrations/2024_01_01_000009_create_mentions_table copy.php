69c1012b11f575fb57f2ca81d551de16811c6dab
debeule <debeulematthias@gmail.com>
2024-05-16 21:12:23 +0200

    Added mentions migration

---
+++ database/migrations/2024_01_01_000009_create_mentions_table copy.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('mentions', function (Blueprint $table): void {
+            $table->id();
+
+            $table->text('context');
+
+            $table->integer('sentiment');
+            $table->timestamp('mention_created_at');
+
+            $table->foreignId('entity_id')->constrained('entities');
+            $table->foreignId('organization_id')->constrained('organizations');
+            $table->foreignId('article_id')->constrained('articles');
+        });
+    }
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
