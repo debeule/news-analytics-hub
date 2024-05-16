49c4e530b65342a5b333edb8d865ea144787bff5
debeule <debeulematthias@gmail.com>
2024-05-16 14:39:27 +0200

    Updated migrations

--- database/migrations/2024_01_01_000005_create_entities_table.php
+++ database/migrations/2024_01_01_000005_create_entities_table.php
@@ -14,9 +14,6 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('type');
-
-            $table->timestamp('created_at')->useCurrent();
         });
     }
     
