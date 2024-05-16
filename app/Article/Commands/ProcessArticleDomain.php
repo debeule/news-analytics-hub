aa2366ec2549344f79934e4034937bf2ca5d2f14
debeule <debeulematthias@gmail.com>
2024-05-16 21:11:04 +0200

    Debugged syncarticle command

--- app/Article/Commands/ProcessArticleDomain.php
+++ app/Article/Commands/ProcessArticleDomain.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 
 namespace App\Article\Commands;
 
-use App\Imports\Article as ExternalArticle;
+use App\Imports\Dtos\Article as ExternalArticle;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
 final class ProcessArticleDomain
 {
     use DispatchesJobs;
 
-    public function __invoke(ExternalArticle $article): void
+    public function __construct(
+        private ExternalArticle $article,
+    ) {}
+
+    public function __invoke(): void
     {
-        $this->dispatchSync(new CreateArticle($article));
+        $this->dispatchSync(new CreateArticle($this->article));
     }
 }
