0fc5c6f9e518bc43eb821778b7825d3bd94643e3
debeule <debeulematthias@gmail.com>
2024-05-16 23:43:21 +0200

    Debugged processing of mention domain

--- app/Mention/Commands/ProcessMentionDomain.php
+++ app/Mention/Commands/ProcessMentionDomain.php
@@ -6,14 +6,20 @@ namespace App\Mention\Commands;
 
 use App\OpenAI\Data;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use App\Article\Queries\ArticleByTitle;
 
 final class ProcessMentionDomain
 {
     use DispatchesJobs;
 
+    private int $articleId;
+
     public function __construct(
         private Data $data,
-    ){}
+        private ArticleByTitle $articleByTitle = new ArticleByTitle,
+    ){
+        $this->articleId = $this->articleByTitle->hasTitle($this->data->article->title)->get()->id;
+    }
 
     public function __invoke(): void
     {
--- app/Mention/Commands/ProcessMentions.php
+++ app/Mention/Commands/ProcessMentions.php
@@ -11,20 +11,16 @@ final class ProcessMentions
 {
     use DispatchesJobs;
 
-    private int $articleId;
-
     public function __construct(
         private collection $mentions,
-        private ArticleByTitle $articleByTitle = new ArticleByTitle,
-    ) {
-        $this->articleId = $this->articleByTitle->hasTitle($this->data->article->title)->get()->id;
-    }
+        private int $articleId,
+    ) {}
 
     public function __invoke(): void
     {
         foreach ($this->mentions as $mention) 
         {
-            $this->dispatchSync(new CreateMention($mention));
+            $this->dispatchSync(new CreateMention($mention, $this->articleId));
         }
     }
 }
