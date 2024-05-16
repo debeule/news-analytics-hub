d282c84850a3e959e820ef9159a78b9c58b9e83c
debeule <debeulematthias@gmail.com>
2024-05-16 21:19:13 +0200

    Setup processing of mention domain

--- app/Mention/Commands/ProcessMentions.php
+++ app/Mention/Commands/ProcessMentions.php
@@ -2,23 +2,29 @@
 
 declare(strict_types=1);
 
-namespace App\Article\Commands;
+namespace App\Mention\Commands;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Support\Collection;
 
 final class ProcessMentions
 {
+    use DispatchesJobs;
+
+    private int $articleId;
+
     public function __construct(
         private collection $mentions,
-        private int $articleId,
-    ) {}
+        private ArticleByTitle $articleByTitle = new ArticleByTitle,
+    ) {
+        $this->articleId = $this->articleByTitle->hasTitle($this->data->article->title)->get()->id;
+    }
 
     public function __invoke(): void
     {
         foreach ($this->mentions as $mention) 
         {
-            $this->dispatchSync(new CreateMention($mention, $this->articleId));
-            $this->dispatchSync(new LinkMentionArticle($mention));
-            $this->dispatchSync(new LinkMentionEntity($mention));
-            $this->dispatchSync(new LinkMentionOrganization($mention));
+            $this->dispatchSync(new CreateMention($mention));
         }
     }
 }
