4da4edba6bd0478da2c05caa86adc2a918184db3
debeule <debeulematthias@gmail.com>
2024-05-20 13:03:05 +0200

    Updated openai prompt for better consistency

--- config/scraping.php
+++ config/scraping.php
@@ -6,23 +6,26 @@ return [
                 "name" => "detijd",
                 "sector" => "source_newspaper",
             ],
-            "2" => [
-                "name" => "demorgen",
-                "sector" => "source_newspaper",
-            ],
+            // "2" => [
+            //     "name" => "demorgen",
+            //     "sector" => "source_newspaper",
+            // ],
     ],
     
     'processing' => [
         'prompt' => "Respond only in json format. Do not exclude data. If a field is not mentioned it is null. Everything in lowercase. Structure the json like:
             
             key: category
-            value: choose most fitting: Politics, Business, Economy, Technology, Science & planet, Entertainment, Sports, Opinion,
+            value: choose most fitting (cannot be null): Politics, Business, Economy, Technology, Science & planet, Entertainment, Sports, Opinion,
            
             key: created_at
             value: date article was published in format yyyy-mm-dd-hh-mm
 
             key: occupations
-            value: every occupation of an entity mentioned
+            value: every occupation occupation mentioned
+                with
+                    name => name of the occupation, 
+                    sector => general sector
 
             key: organizations
             value: every mentioned organizations and company 
@@ -31,19 +34,19 @@ return [
                     sector => general sector
 
             key: entities
-            value: every person mentioned 
+            value: every mention of an organization, company or entity
                 with
-                    name, 
-                    occupation, 
-                    name of linked organization from organizations key.
+                    name => name of mentioned entity / organization / company, 
+                    occupation => occupation, 
+                    organization => name of linked organization from organizations key.
 
             key: mentions
             value: all instances of an entity or organization being mentioned
                 with
-                    context of mention (15 words), 
-                    positivity score from 1 - 16
-                    name of mentioned entity,
-                    name of mentioned organization, 
+                    context => context of mention (15 words), 
+                    sentiment => score from 1 - 16
+                    entityName => name of mentioned entity (ensure entity is included in entities key),
+                    organizationName => name of mentioned organization (ensure organization is included in organizations key), 
         ",
     ]
 ];
