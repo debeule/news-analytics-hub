8c7a133d3dc155f6670b65a3501a5492a321bd12
debeule <debeulematthias@gmail.com>
2024-05-22 22:26:15 +0200

    Seperated prompt from scraping config

---
+++ config/Processing.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'prompt' => "Respond only in JSON format. Fields cannot be null unless specified. Ensure all data is lowercase. Structure the JSON as follows:
+
+        key: category
+        value: choose the most fitting category: politics, business, economy, technology, science & planet, entertainment, sports, opinion, media.
+    
+        key: created_at
+        value: date article was published in the format yyyy-mm-dd-hh-mm.
+    
+        key: author
+        value: name of the author (nullable)
+
+        key: occupations
+        value: list of all mentioned occupations, each with:
+            - name: name of the occupation
+            - sector: general sector
+    
+        key: organizations
+        value: list of all mentioned organizations and companies, each with:
+            - name: organization name
+            - sector: general sector
+    
+        key: entities
+        value: list of all mentioned entities and organizations, each with:
+            - name: name of mentioned entity/organization/company
+            - occupation: occupation (nullable)
+            - organization: name of linked organization from 'organizations' key (nullable)
+    
+        key: mentions
+        value: all instances of entities or organizations being mentioned, each with:
+            - context: context of mention (up to 15 words)
+            - sentiment: score from 1 to 16
+            - entity_name: name of mentioned entity (ensure entity is included in 'entities' key) (nullable if organization is mentioned)
+            - organization_name: name of mentioned organization (ensure organization is included in 'organizations' key) (nullable if entity is mentioned)",
+];
