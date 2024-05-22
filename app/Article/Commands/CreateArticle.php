7e88ccc237ef55c07294419ade103c68b276d602
debeule <debeulematthias@gmail.com>
2024-05-22 22:28:22 +0200

    Implemented new article setup

--- app/Article/Commands/CreateArticle.php
+++ app/Article/Commands/CreateArticle.php
@@ -6,15 +6,19 @@ declare(strict_types=1);
 namespace App\Article\Commands;
 
 use App\Article\Article as DbArticle;
-use App\Imports\Dtos\Article;
+use App\Imports\Dtos\ProcessedArticle as Article;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use App\Entity\Queries\EntityByName;
+use App\Entity\Queries\OrganizationByName;
 
 final class CreateArticle
 {
     use DispatchesJobs;
 
     public function __construct(
-        public Article $article
+        public Article $article,
+        private EntityByName $entityByName = new EntityByName(),
+        private OrganizationByName $organizationByName = new OrganizationByName(),
     ) {}
 
     public function handle(): bool
@@ -34,7 +38,7 @@ final class CreateArticle
         $newArticle->word_count = str_word_count($this->article->fullContent());
         $newArticle->article_created_at = $article->createdAt();
         
-        $newArticle->author_id = $article->authorId();
+        $newArticle->author_id = $this->entityByName->hasName($article->author())->find()->id ?? null;
         $newArticle->organization_id = $article->organizationId();
 
         return $newArticle;
