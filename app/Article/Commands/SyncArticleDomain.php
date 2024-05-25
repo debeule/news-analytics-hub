9fc758c7dc614861c4ca831167fde42af9506518
debeule <debeulematthias@gmail.com>
2024-05-25 10:33:33 +0200

    Moved foreach newspaper scrape list to syncarticledomain

--- app/Article/Commands/SyncArticleDomain.php
+++ app/Article/Commands/SyncArticleDomain.php
@@ -5,13 +5,24 @@ declare(strict_types=1);
 namespace App\Article\Commands;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Scraper\Commands\ScrapeArticle;
+use App\Imports\Values\GuzzleResponse;
+use App\Entity\Organization;
 
 final class SyncArticleDomain
 {
-    use DispatchesJobs;
+    use DispatchesJobs, InteractsWithQueue, Queueable, SerializesModels, Batchable;
 
     public function __invoke(): void
     {
-        $this->DispatchSync(new SyncArticles);
+        foreach (Organization::where('sector', 'source_newspaper')->get() as $organization) 
+        {
+            $this->dispatch(new SyncArticles($organization));
+        }
     }
 }
