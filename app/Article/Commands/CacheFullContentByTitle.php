29faa295c77d589d61c4cb012aa6d069b92bc2b6
debeule <debeulematthias@gmail.com>
2024-05-25 16:55:03 +0200

    Added caching command and query for communicating between chianed jobs

---
+++ app/Article/Commands/CacheFullContentByTitle.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Article\Commands;
+
+use App\Imports\Dtos\Article as ExternalArticle;
+use Illuminate\Support\Facades\Cache;
+
+class CacheFullContentByTitle
+{
+    public function __construct(
+        public string $articleTitle = '',
+        public int $organizationId = 0,
+        public string $fullContent = '',
+    ) {}
+
+    public static function setup(ExternalArticle $article, string $fullContent): self
+    {
+        return new self(
+            $article->title,
+            $article->organizationId,
+            $fullContent,
+        );
+    }
+
+    public function CacheData(): void
+    {
+        Cache::put('article_full_content_' . $this->organizationId . '_' . $this->articleTitle, $this->fullContent);
+    }
+
+    public function execute(): void
+    {
+        $this->CacheData();
+    }
+}
---
+++ app/Article/Queries/CacheFullContentByTitle.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Article\Queries;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+
+class CacheFullContentByTitle
+{
+    public function __construct(
+        public string $articleTitle = '',
+        public int $organizationId = 0,
+    ) {}
+
+    public function query(): string
+    {
+        return Cache::get('article_full_content_' . $this->organizationId . '_' . $this->articleTitle);
+    }
+
+    public function fromArticleTitle(string $articleTitle): self
+    {
+        return new self(
+            $articleTitle,
+            $this->organizationId,
+        );
+    }
+
+    public function fromOrganizationId(int $organizationId): self
+    {
+        return new self(
+            $this->articleTitle,
+            $organizationId,
+        );
+    }
+
+    public function get(): string
+    {
+        return $this->query();
+    }
+
+    public function find(): ?string
+    {
+        try 
+        {
+            return $this->get();
+        } 
+        catch (ModelNotFoundException) 
+        {
+            return null;
+        }
+    }
+}
