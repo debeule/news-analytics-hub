ac70f575f7926609eba9a6a7a7f473b053586d7e
debeule <debeulematthias@gmail.com>
2024-05-25 10:35:56 +0200

    Added test for articlesDiff

---
+++ app/Article/Queries/ArticlesDiffTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Article\Queries;
+
+use App\Article\Commands\CreateArticle;
+use App\Scraper\Article;
+use App\Article\Article as DbArticle;
+use App\Testing\TestCase;
+use Database\Scraper\ArticleFactory as ScraperArticleFactory;
+use Database\Factories\EntityFactory;
+use Database\Factories\OrganizationFactory;
+use PHPUnit\Framework\Attributes\Test;
+use Carbon\CarbonImmutable;
+use App\Imports\Queries\ExternalArticles;
+
+final class ArticlesDiffTest extends TestCase
+{
+    #[Test]
+    public function itReturnsCorrectAdditions2(): void
+    {
+        $entity = EntityFactory::new()->create();
+        $organization = OrganizationFactory::new()->create();
+
+        $externalArticles = ScraperArticleFactory::new()
+            ->withOrganizationId($organization->id)
+            ->count(3)
+            ->create();
+
+        DbArticle::create([
+            'title' => $externalArticles->first()->title,
+             'word_count'=> '100',
+            'full_content' => 'abc',
+            'url' => 'www.example.com',
+            'article_created_at' => CarbonImmutable::now(),
+            'category' => 'business',
+            'author_id' => $entity->id,
+            'organization_id' => $organization->id
+        ]);
+        
+        $externalArticlesMock = $this->createMock(ExternalArticles::class);
+        $externalArticlesMock->method('get')->willReturn($externalArticles);
+
+        $articlesDiff = new ArticlesDiff($externalArticlesMock);
+
+        $result = $articlesDiff($organization->id)->additions();
+
+        $this->assertInstanceOf(Article::class, $result->first());
+        $this->assertEquals(2, $result->count());
+    }
+}
