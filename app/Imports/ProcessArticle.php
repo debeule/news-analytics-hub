f2b3c597402fdc5dd567b6035df028a39dc5c604
debeule <debeulematthias@gmail.com>
2024-05-25 16:54:20 +0200

    Seperated processing and scraping into seperate jobs

--- app/Imports/ProcessArticle.php
+++ app/Imports/ProcessArticle.php
@@ -10,6 +10,7 @@ use App\Imports\Dtos\Article as ExternalArticle;
 use App\Mention\Commands\ProcessMentionDomain;
 use App\OpenAi\Commands\CreateDataObject;
 use App\OpenAi\Commands\ProcessData;
+use App\Article\Article;
 
 use App\OpenAi\Data;
 use Illuminate\Bus\Batchable;
@@ -20,6 +21,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Scraper\Commands\ScrapeArticle;
 use App\Imports\Values\GuzzleResponse;
+use App\Article\Queries\CacheFullContentByTitle;
 
 class ProcessArticle implements ShouldQueue
 {
@@ -29,33 +31,29 @@ class ProcessArticle implements ShouldQueue
 
     public function __construct(
         public ExternalArticle $article,
-    ){}
+        public CacheFullContentByTitle $fullContentQuery = new CacheFullContentByTitle,
+    ){
+        $this->fullContentQuery = $this->fullContentQuery
+            ->fromArticleTitle($this->article->title)
+            ->fromOrganizationId($this->article->organizationId);
+    }
 
     public function handle(): void
     {
-        $this->article->fullContent = $this->scrapeArticleContent();
 
-        $data = $this->getData();
+        $data = $this->getData($this->article);
 
         $this->dispatchSync(new ProcessEntityDomain($data));
         $this->dispatchSync(new ProcessArticleDomain($data->article()));
         $this->dispatchSync(new ProcessMentionDomain($data));
     }
 
-    public function getData(): Data
+    public function getData(ExternalArticle $article): Data
     {
-        $processData = new ProcessData($this->article->fullContent());
+        $this->article->fullContent = $this->fullContentQuery->find();
+
+        $processData = new ProcessData($this->article->fullContent);
 
         return CreateDataobject::fromArray($processData->get(), $this->article)->toDataObject();
-    }
-
-    public function scrapeArticleContent(): string
-    {
-        $response = ScrapeArticle::setup($this->article)->get();
-        $fullContent = GuzzleResponse::fromResponse($response)->extractScraperResponse();
-
-        if ($fullContent == '') throw new \Exception('Failed to scrape article content');
-
-        return $fullContent;
     }
 }
---
+++ app/Imports/ScrapeArticle.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Imports;
+
+use App\Article\Commands\ProcessArticleDomain;
+use App\Entity\Commands\ProcessEntityDomain;
+use App\Imports\Dtos\Article as ExternalArticle;
+use App\Mention\Commands\ProcessMentionDomain;
+use App\OpenAi\Commands\CreateDataObject;
+use App\OpenAi\Commands\ProcessData;
+
+use App\OpenAi\Data;
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Scraper\Commands\ScrapeArticle as ScrapeArticleCommand;
+use App\Imports\Values\GuzzleResponse;
+use App\Article\Commands\CacheFullContentByTitle;
+
+class ScrapeArticle implements ShouldQueue
+{
+    use DispatchesJobs, InteractsWithQueue, Queueable, SerializesModels, Batchable;
+
+    public $tries = 4;
+
+    public function __construct(
+        public ExternalArticle $article,
+    ){}
+
+    public function handle(): void
+    {
+        $this->article->fullContent = $this->scrapeArticleContent();
+        
+        CacheFullContentByTitle::setup($this->article, $this->article->fullContent)->execute();
+    }
+
+    public function scrapeArticleContent(): string
+    {
+        $response = ScrapeArticleCommand::setup($this->article)->get();
+        $fullContent = GuzzleResponse::fromResponse($response)->extractScraperResponse();
+
+        if ($fullContent == '') throw new \Exception('Failed to scrape article content');
+
+        return $fullContent;
+    }
+}
