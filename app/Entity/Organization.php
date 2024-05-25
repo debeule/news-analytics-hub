1082c2dfd6653ba9874b86fa61b9d4f9ed0c2db6
debeule <debeulematthias@gmail.com>
2024-05-25 18:18:58 +0200

    Added missing imports

--- app/Entity/Organization.php
+++ app/Entity/Organization.php
@@ -6,6 +6,8 @@ namespace App\Entity;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Article\Article;
+use App\Location\Location;
 
 class Organization extends Model
 {
